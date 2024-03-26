6bdf26239f0a7359e826bc994f9e41fb16a43080
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-03-26 08:54:18 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,8 +26,6 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    domain: "https://dostfrnd.onrender.com",
-    SameSite: 'Lax'
   };
 
   console.log("cookie : ",cookieOptions);
