f73937b3c18f0d83571a5d5da8691c4604aece1f
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-03-26 08:11:56 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,11 +26,12 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    sameSite: 'Lax'
   };
 
   console.log("cookie : ",cookieOptions);
 
-  // if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+  cookieOptions.secure = true;
 
   res.cookie("jwt", token, cookieOptions);
 
