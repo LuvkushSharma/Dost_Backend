cf076dc7123a57030460d359e55d7ace953c32bb
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-03-26 07:57:45 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,6 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    domain: ".onrender.com",
   };
 
   console.log("hello2");
