8e889efc54830c60cc9be44c25d186269721291c
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-03-26 21:37:17 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,12 +26,12 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    path: '/',
+    domain: ".onrender.com",
   };
 
-  if (process.env.NODE_ENV === 'production') {
-    cookieOptions.secure = true;
-  }
+  // if (process.env.NODE_ENV === 'production') {
+  //   cookieOptions.secure = true;
+  // }
 
   // Remove password from output
   user.password = undefined;
