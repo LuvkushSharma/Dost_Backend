d4b8f8889b7d7dfa9ea4e2a1605fe9b850d4f727
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-03-27 20:58:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
 
+
+app.use(cookieParser());
+
 const corsOptions = { 
     credentials: true,
     origin: "*",
@@ -21,9 +24,6 @@ app.use(cors(corsOptions))
 
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
-
-
-app.use(cookieParser());
 
 app.use (express.static (path.join (__dirname , 'public')));
 
