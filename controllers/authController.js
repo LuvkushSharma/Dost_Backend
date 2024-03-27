545411337caea0c55aca2d69e35359f8f239a0d8
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-03-27 15:34:56 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,6 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
   };
 
   // if (process.env.NODE_ENV === 'production') {
