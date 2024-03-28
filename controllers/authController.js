70f9c4023a9ca09b75d02ca584fb95e6883e1f57
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-03-28 09:59:25 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -104,18 +104,17 @@ exports.logout = (req, res) => {
 exports.protect = catchAsync(async (req, res, next) => {
   let token;
 
-  console.log("req.cookies : ",req.cookies?.jwt);
+  console.log("req.cookie : ",req.cookie?.jwt);
 
-  // if (
-  //   req.headers?.authorization &&
-  //   req.headers?.authorization.startsWith("Bearer")
-  // ) {
-  //   token = req.headers.authorization.split(" ")[1];
-  // } else if (req.cookies?.jwt) {
-  //   token = req.cookies?.jwt;
-  // }
+  if (
+    req.headers?.authorization &&
+    req.headers?.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookie?.jwt) {
+    token = req.cookie?.jwt;
+  }
 
-  token = localStorage.getItem("jwt");
 
   console.log("token : ",token);
 
