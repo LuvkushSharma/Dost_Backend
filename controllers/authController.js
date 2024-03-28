9613eb9d8cdee7022dea945b7c660bc311afc597
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-03-28 10:25:52 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,8 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
+    httpOnly: true,
+    domain: ".onrender.com"
   };
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
